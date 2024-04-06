58b188b4f1dd8c012a14d2d99451eb3a72e35bc9
Lyuben Petrov <lyuben.y.petrov@gmail.com>
2024-04-06 17:00:36 +0300

    Implement basic Pratt parsing

--- ast/ast.go
+++ ast/ast.go
@@ -137,3 +137,24 @@ func (pe *PrefixExpression) String() string {
 
 	return out.String()
 }
+
+type InfixExpression struct {
+	Token    token.Token // the prefix Token, e.g. !
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (pe *InfixExpression) expressionNode()      {}
+func (pe *InfixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Left.String())
+	out.WriteString(" " + pe.Operator + " ")
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
